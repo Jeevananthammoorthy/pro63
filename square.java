7668578909d3435a44bff950e174cb497b483b01
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2018-03-26 21:30:41 +0530

    Create square.java

---
+++ square.java
@@ -0,0 +1,23 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+
+class square {
+	public static void main (String[] args) {
+		Scanner in=new Scanner(System.in);
+		int index=0,count=0;
+		  int[] array=new int[8];
+		  for(index=0;index<8;index++)
+		    array[index]=in.nextInt();
+		   Arrays.sort(array);
+		   System.out.println(Arrays.toString(array));
+		   for(index=0;index<4;index++)
+		   {
+		       if((2*array[index])==(array[index+4]))
+		       {
+		         count++;
+		       }
+		   }
+		   System.out.println(count==4?"yes":"no");
+	}
+}
